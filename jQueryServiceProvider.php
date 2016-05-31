38e0fd8a2bad87054ca57042f101a75ee893c5da
Stephen Lind <slind@ualberta.ca>
2016-05-31 14:33:54 -0600

    service provider to ensure install works right

---
+++ jQueryServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace linds14sr20det\jQueryGantt;
+
+use Depotwarehouse\Toolbox\FrameworkIntegration\Laravel\ViewComposers\ErrorPartialComposer;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\View\Factory;
+
+class jQueryGanttServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/Views' => base_path('public/vendor/linds4sr20det')
+        ]);
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
+
